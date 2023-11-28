3a98fd5f68c0b826ee761f9999f003690c8af72f
Stevecool023 <karanjastephenn@gmail.com>
2023-11-28 15:09:47 +0300

    arrow functions

--- arrow_functions.js
+++ arrow_functions.js
@@ -1,7 +1,11 @@
 // ES5
-var x = function(x, y) {
+var multiplyES5 = function(x, y) {
 	return x * y;
 }
 
 // ES6
-const x = (x, y) => x * y;
+const multiplyES6 = (x, y) => x * y;
+
+// Example usage and logging
+console.log(multiplyES5(5, 3)); // Output: 15
+console.log(multiplyES6(5, 3)); // Output: 15
