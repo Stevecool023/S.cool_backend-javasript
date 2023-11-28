0cfa838830d3485a4f952282c23609ae13144e90
Stevecool023 <karanjastephenn@gmail.com>
2023-11-28 15:34:28 +0300

    spread (...) operator

---
+++ 1-spread...operator.js
@@ -0,0 +1,6 @@
+// The function can be used to expand an iterable into more arguments for function calls
+
+
+const numbers = [23, 55, 21, 87, 56];
+let maxValue = Math.max(...numbers);
+console.log(maxValue);
