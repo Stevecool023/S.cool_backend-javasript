e263ad57ccb1ae90b2a3e405e4a3daac6f230ca7
Stevecool023 <karanjastephenn@gmail.com>
2023-11-28 14:49:03 +0300

    JS const

---
+++ 0-const.js
@@ -0,0 +1,14 @@
+// The 'const' keyword allows you to declare a constant (a JavaScript variable with a constant value).
+
+// Constants are similar to let variables, except that the value cannot be changed.
+
+var x = 3; {
+	console.log(x);  // Here x is 3
+}
+
+{
+	const x = 10;  // Here x is 10
+	console.log(x);
+}
+
+console.log(x);  // Here x is 3
