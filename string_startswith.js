143e55bec8ac08b6ccb06d5f583a3488fcbca3bd
Stevecool023 <karanjastephenn@gmail.com>
2023-11-30 13:25:11 +0300

    string startsWith method

---
+++ string_startswith.js
@@ -0,0 +1,5 @@
+/* The startsWith() method returns true if a string begins with a specified value, otherwise false */
+// Example
+let text = "Hello world, welcome to the universe.";
+console.log(text.startsWith("Hello")); // Returns true
+console.log(text.startsWith("world")); // Returns false
