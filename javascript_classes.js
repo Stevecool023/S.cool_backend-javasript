34505cccc607921f694651708321e3d2098113f5
Stevecool023 <karanjastephenn@gmail.com>
2023-11-30 10:40:02 +0300

    JavaSctipt Classes

--- javascript_classes.js
+++ javascript_classes.js
@@ -11,6 +11,11 @@ class Car {
 	}
 }
 
-const myCar = new Car("Toyota", 2022);
-console.log(myCar.name + ":" + " " + myCar.year);
+const myCar1 = new Car("Toyota", 2022);
+const myCar2 = new Car("Ford", 2014);
+const myCar3 = new Car("Audi", 2019);
+
+console.log(myCar1.name + ":" + " " + myCar1.year);
+console.log(myCar2.name + ":" + " " + myCar2.year);
+console.log(myCar3.name + ":" + " " + myCar3.year);
 // console.log(myCar.year);
