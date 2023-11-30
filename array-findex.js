b22b92049d281c223d30048af1837edff389766f
Stevecool023 <karanjastephenn@gmail.com>
2023-11-30 16:22:06 +0300

    Array findIndex()

---
+++ array-findex.js
@@ -0,0 +1,15 @@
+/*
+ * The find index() method returns the index of the first array element
+ * that passes a text function.
+ * This example finds the index of the first element
+ * that is larger than 18
+ */
+
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.findIndex(myFunction);
+
+function myFunction(value, index, array) {
+	return value > 18;
+}
+
+console.log(first);
