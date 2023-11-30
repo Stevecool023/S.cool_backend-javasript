ef7fc8cc06c9b58b43e95ac15f6dac3bad8c3ab4
Stevecool023 <karanjastephenn@gmail.com>
2023-11-30 19:25:07 +0300

    break

---
+++ break_in_switch_statements.js
@@ -0,0 +1,13 @@
+const food = 'sushi'
+
+switch (food) {
+	case "sushi":
+		console.log("Sushi is originally from Japan.");
+		break;
+	case "pizza":
+		console.log("Pizza is originally form Itally.");
+		break;
+	default:
+		console.log("I have never heard of that dish.");
+		break;
+}
