1b7512d26b7f4d679c4f54224bcb8c160fe2a4a1
Stevecool023 <karanjastephenn@gmail.com>
2023-11-30 16:56:44 +0300

    New Number Methods

--- 1-number_issafeinteger.js
+++ 1-number_issafeinteger.js
@@ -2,6 +2,8 @@
  * The Number.isSafeInteger() Method
  * A safe integer is an integer that can be exactly represented as a double precision number
  * This method returns true if the argument is a safe integer
+ * Safe integers are all integers from -(2^53 - 1) to +(2^53 - 1).
+ * This is safe: 9007199254740991. This is not safe: 9007199254740992.
  */
 
 // Example
