d750d224fb052210b3a7f0e77ce521195b5e4be0
Stevecool023 <karanjastephenn@gmail.com>
2023-11-30 13:33:02 +0300

    string.endsWith() method

---
+++ string-endswith.js
@@ -0,0 +1,5 @@
+/* The endsWith() method returns true if a string ends with a specified value, otherwise false */
+
+var text = "John Doe";
+console.log(text.endsWith("Doe")); // Returns true
+console.log(text.endsWith("John")); // Returns false
