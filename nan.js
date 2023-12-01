da51d20f9958d6f9ed1740946b006e9464f6a7f1
Stevecool023 <karanjastephenn@gmail.com>
2023-12-01 10:14:10 +0300

    ES6 nan/default/parameters

---
+++ nan.js
@@ -0,0 +1,12 @@
+function multiply(a, b) {
+	return a * b;
+}
+
+console.log(multiply(5, 2));
+/* Expected output: 10 */
+
+console.log(multiply(5 * 2, 3));
+/* Expected output: 30 */
+
+console.log(multiply(5));
+/* Expected output: NaN ! */
